e0632a6aa2a4673af205f227ce6e62f599ec2f86
naitokosuke <kosuke.naito.engineer@gmail.com>
2025-05-06 20:05:46 +0900

    feat: update cspell dictionary, add cover image, and modify slides for 東葛.dev presentation

--- prettier.config.js
+++ prettier.config.js
@@ -3,9 +3,17 @@ const config = {
   tabWidth: 2,
   semi: true,
   singleQuote: false,
-  plugins: [
-    "prettier-plugin-slidev",
-  ]
-}
+  plugins: ["prettier-plugin-slidev"],
+  plugins: ["prettier-plugin-slidev"],
+  overrides: [
+    {
+      files: ["slides.md", "pages/*.md"],
+      options: {
+        parser: "slidev",
+        // plugins: ["prettier-plugin-slidev"],
+      },
+    },
+  ],
+};
 
 export default config;
